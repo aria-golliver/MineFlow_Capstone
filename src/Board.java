7976a6cc170e73e4093858396f8cb382b8d01a8f
Aaron Golliver <aaron.golliver@gmail.com>
2012-03-20 21:48:59 -0700

    Board generates just fine now,

--- src/Board.java
+++ src/Board.java
@@ -21,7 +21,7 @@ public class Board {
 				board[x][y].put("mine?", false);
 				board[x][y].put("flagged?", false);
 				board[x][y].put("uncovered?", false);
-				board[x][y].put("surrounding_mines?", 0);
+				board[x][y].put("surrounding_mines", 0);
 			}
 		}
 		int mines_left = total_mines;
@@ -35,14 +35,49 @@ public class Board {
 			}
 		}
 		
-		for(int x = 0; x < width; x++)
-			for(int y = 0; y < height; y++)
-				if((boolean) board[x][y].get("mine?"))
-					System.out.println("("+x+","+y+")");
-			
+
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				board[x][y].put("surrounding_mines", get_surrounding_mines(x,y));
+			}
+		}
+
+		for(int y = 0; y < width; y++){
+			for(int x = 0; x < height; x++){
+				if((boolean) board[x][y].get("mine?")){
+					System.out.print(" X");
+				} else {
+					System.out.print(" .");
+				}
+			}
+			System.out.println();
+		}
 		
+		for(int y = 0; y < width; y++){
+			for(int x = 0; x < height; x++){
+				System.out.print(" " + board[x][y].get("surrounding_mines"));
+			}
+			System.out.println();
+		}
 	}
 	
+	public int get_surrounding_mines(int x, int y){
+		int surrounding_mines = 0;
+		if(is_mine(x-1,y-1)) surrounding_mines++;
+		if(is_mine(x  ,y-1)) surrounding_mines++;
+		if(is_mine(x+1,y-1)) surrounding_mines++;
+		if(is_mine(x+1,y  )) surrounding_mines++;
+		if(is_mine(x+1,y+1)) surrounding_mines++;
+		if(is_mine(x  ,y+1)) surrounding_mines++;
+		if(is_mine(x-1,y+1)) surrounding_mines++;
+		if(is_mine(x-1,y  )) surrounding_mines++;
+		return surrounding_mines;
+	}
+	
+	private boolean is_mine(int x, int y){
+		if(x >= width || x < 0 || y >= height || y < 0) return false;
+		return (boolean) board[x][y].get("mine?");
+	}
 	
 	public static void main(String[] args){
 		Board test = new Board();
