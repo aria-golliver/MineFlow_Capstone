706155d5d7d8d53e1510eb0b647f8015ed883375
Aaron Golliver <aaron.golliver@gmail.com>
2012-03-21 19:25:09 -0700

    Logic is mostly done, needs testing and the random-case to be finished

--- src/MinesweeperThread.java
+++ src/MinesweeperThread.java
@@ -16,27 +16,9 @@ public class MinesweeperThread extends Thread {
 			for(int x = 0; x<board.width; x++){
 				for(int y = 0; y<board.width; y++){
 					if(!(boolean) board.board[x][y].get("revealed?")){
-						int surrounding_empty_spaces = 0;
-						int surrounding_flags = 0;
-						//BAD
-						if(!(boolean) board.board[x-1][y-1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x  ][y-1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x+1][y-1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x+1][y  ].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x+1][y+1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x  ][y+1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x-1][y+1].get("revealed?")) surrounding_empty_spaces++;
-						if(!(boolean) board.board[x-1][y  ].get("revealed?")) surrounding_empty_spaces++;
 						
-
-						if((boolean) board.board[x-1][y-1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x  ][y-1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x+1][y-1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x+1][y  ].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x+1][y+1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x  ][y+1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x-1][y+1].get("flagged?")) surrounding_flags++;
-						if((boolean) board.board[x-1][y  ].get("flagged?")) surrounding_flags++;
+						int surrounding_empty_spaces = board.total_arround_cell(x, y, "revealed?", false);
+						int surrounding_flags = board.total_arround_cell(x, y, "revealed?");
 						
 						if (surrounding_empty_spaces == (int) board.board[x][y].get("surrounding_mines")){
 							made_a_move = board.right_click(x-1, y-1)  || made_a_move;
@@ -49,6 +31,16 @@ public class MinesweeperThread extends Thread {
 							made_a_move = board.right_click(x, y-1)    || made_a_move;
 						}
 					}
+				}
+			}
+
+			for(int x = 0; x<board.width; x++){
+				for(int y = 0; y<board.width; y++){
+					int surrounding_empty_spaces = board.total_arround_cell(x, y, "revealed?", false);
+					int surrounding_flags = board.total_arround_cell(x, y, "revealed?");
+					
+					if(surrounding_empty_spaces > 0 && surrounding_flags == board.view_cell(x, y));
+					made_a_move = board.middle_click(x, y) || made_a_move;
 				}
 			}
 		}
