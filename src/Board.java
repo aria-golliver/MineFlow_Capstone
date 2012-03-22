73598dc5dc5a775d5304d08e030468927d8d1e95
Aaron Golliver <aaron.golliver@gmail.com>
2012-03-22 10:14:05 -0700

    Fixed a serious bug with flagging. Forgot puncutation w/ keys x.x I I much perfer clojure keys for hashmaps but I'll get used to it eventually

--- src/Board.java
+++ src/Board.java
@@ -63,11 +63,7 @@ public class Board {
 		}
 		
 
-		for(int x = 0; x < width; x++){
-			for(int y = 0; y < height; y++){
-				board[x][y].put("surrounding_mines", total_arround_cell(x,y, "mine?"));
-			}
-		}
+		generate_surrounding_mines_metadata();
 
 		/*
 		for(int y = 0; y < width; y++){
@@ -87,6 +83,14 @@ public class Board {
 			}
 			System.out.println();
 		}*/
+	}
+
+	private void generate_surrounding_mines_metadata() {
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				board[x][y].put("surrounding_mines", total_arround_cell(x,y, "mine?"));
+			}
+		}
 	}
 	
 	public int total_arround_cell(int x, int y, String key){
@@ -124,12 +128,17 @@ public class Board {
 	
 	public boolean left_click(int x, int y){
 		if(out_of_bounds(x,y) || won || lost) return false;
+		
 		System.out.println("CLICK (" +x+","+y+")");
-		return reveal(x,y);
+		boolean made_a_move = false;
+		if(!(boolean) board[x][y].get("flagged?")) made_a_move = reveal(x,y);
+		print_board();
+		return made_a_move;
 	}
 	
 	public boolean middle_click(int x, int y){
 		if(out_of_bounds(x,y) || won || lost) return false;
+		System.out.println("MIDDLE_CLICK (" +x+","+y+")");
 		boolean made_a_move = false;
 		made_a_move = left_click(x-1,y-1) || made_a_move;
 		made_a_move = left_click(x  ,y-1) || made_a_move;
@@ -139,17 +148,28 @@ public class Board {
 		made_a_move = left_click(x  ,y+1) || made_a_move;
 		made_a_move = left_click(x-1,y+1) || made_a_move;
 		made_a_move = left_click(x-1,y  ) || made_a_move;
+		print_board();
 		return made_a_move;
 	}
 	
 	public boolean right_click(int x, int y){
-		return toggle_flag(x,y);
+		
+		if(toggle_flag(x,y)) {
+			System.out.println("FLAG " + "("+x+","+y+")");
+			print_board();
+			return true;
+		} return false;
+		
+		
 	}
 	
 	private boolean toggle_flag(int x, int y){
 		if(out_of_bounds(x,y)) return false;
-		board[x][y].put("flagged", !((boolean) board[x][y].get("flagged?")));
-		return true;
+		if(!((boolean) board[x][y].get("uncovered?"))){
+			board[x][y].put("flagged?", true);
+			return true;
+		}
+		return false;
 	}
 	
 	private boolean reveal(int x, int y){
@@ -170,6 +190,7 @@ public class Board {
 						}
 					}
 				}
+				generate_surrounding_mines_metadata();
 				reveal(x,y);
 				return true;
 			}
@@ -177,7 +198,7 @@ public class Board {
 			return true;
 		}
 		first_move = false;
-		print_board();
+		//print_board();
 		// if you didn't hit a mine...
 		if((boolean) board[x][y].get("uncovered?")){
 			return false;
@@ -208,8 +229,9 @@ public class Board {
 	}
 
 	public void print_board(){
-		System.out.println("------------");
+		System.out.println("    0 1 2 3 4 5 6 7 8 9");
 		for(int y = 0; y < height; y++){
+			System.out.print(y+"| ");
 			for(int x = 0; x < width; x++){
 				if((boolean) board[x][y].get("flagged?")){
 					System.out.print(" F");
@@ -218,12 +240,12 @@ public class Board {
 				} else if((boolean) board[x][y].get("uncovered?")){
 					int surrounding_mines = (int) board[x][y].get("surrounding_mines");
 					if(surrounding_mines == 0){
-						System.out.print("  ");
+						System.out.print(" .");
 					} else {
 						System.out.print(" " + surrounding_mines);
 					}
 				} else {
-					System.out.print(" .");
+					System.out.print(" #");
 				}
 			}
 			System.out.println();
@@ -240,21 +262,24 @@ public class Board {
 		Board test = new Board();
 		test.new_game();
 		test.print_board();
-		test.left_click(3, 3);
+		test.right_click(0,0);
+		System.out.println(test.board[0][0].get("flagged?"));
+		//test.left_click(3, 3);
 		System.out.println("------------");
 		test.print_board();
-		test.left_click(4, 8);
+		//test.left_click(4, 8);
 		System.out.println("------------");
 		test.print_board();
-		test.left_click(6, 2);
+		//test.left_click(6, 2);
 		System.out.println("------------");
 		test.print_board();
 	}
 
 	public int view_cell(int x, int y) {
 		if(out_of_bounds(x,y)) return -1;
-		if((boolean) board[x][y].get("uncovered?"))
-			return (int) board[x][y].get("surrounding_mines"); 
+		if((boolean) board[x][y].get("uncovered?")){
+			return (int) board[x][y].get("surrounding_mines");
+		}
 		return -1;
 	}
 	
--- src/MinesweeperThread.java
+++ src/MinesweeperThread.java
@@ -20,13 +20,14 @@ public class MinesweeperThread extends Thread {
 		while(!board.lost && !board.won){
 			boolean made_a_move = false;
 			for(int x = 0; x<board.width; x++){
+				//System.out.println("1");
 				for(int y = 0; y<board.height; y++){
 					if(board.view_cell(x, y) > 0){
 						
 						int surrounding_empty_spaces = board.total_arround_cell(x, y, "uncovered?", false);
-						int surrounding_flags = board.total_arround_cell(x, y, "uncovered?");
+						int surrounding_flags = board.total_arround_cell(x, y, "flagged?");
 						
-						if (surrounding_empty_spaces == (int) board.board[x][y].get("surrounding_mines")){
+						if (surrounding_empty_spaces == board.view_cell(x,y)){
 							made_a_move = board.right_click(x-1, y-1) || made_a_move;
 							made_a_move = board.right_click(x-1, y  ) || made_a_move;
 							made_a_move = board.right_click(x-1, y+1) || made_a_move;
@@ -41,24 +42,31 @@ public class MinesweeperThread extends Thread {
 			}
 
 			for(int x = 0; x<board.width; x++){
+				//System.out.println("2");
 				for(int y = 0; y<board.height; y++){
 					int surrounding_empty_spaces = board.total_arround_cell(x, y, "uncovered?", false);
 					int surrounding_flags = board.total_arround_cell(x, y, "uncovered?");
 					
-					if(surrounding_empty_spaces > 0 && surrounding_flags == board.view_cell(x, y));
-					made_a_move = board.middle_click(x, y) || made_a_move;
+					if(surrounding_empty_spaces > 0 && 
+					   surrounding_flags == board.view_cell(x, y) && 
+					   (boolean) board.board[x][y].get("uncovered?")){
+							made_a_move = board.middle_click(x, y) || made_a_move;
+					}
 				}
 			}
 			while(!made_a_move){
 				int x = (int) (Math.random() * board.width);
 				int y = (int) (Math.random() * board.height);
+				System.out.println("guessing (" +x+","+y+")");
 				made_a_move = board.left_click(x, y);
 			}
 			if(board.won){
 				System.out.println("WON!");
+				board.print_board();
 				while(true){}
 			} else if(board.lost){
 				System.out.println("LOST!");
+				board.print_board();
 				while(true){}
 			}
 		}
